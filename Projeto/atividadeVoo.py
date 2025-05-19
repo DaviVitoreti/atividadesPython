c7a391eeca617151aac19958fcce09699cbf0e8c
DaviVitoreti <DaviVitoreti@outlook.com>
2025-05-19 09:11:09 -0300

    Foram implemetadas todas as funcionalidades em atividadeVoo.py

--- Projeto/atividadeVoo.py
+++ Projeto/atividadeVoo.py
@@ -1,8 +1,8 @@
 usuarios = {}
 voo = {}
+voos = {}
 passageiros = []
 id = 1
-menu = ""
     
 # Função para cadastrar um novo usuário
 def cadastro_usuario():
@@ -16,7 +16,7 @@ def cadastro_usuario():
         telefone = input("Digite o seu telefone (XX)(XXXXX-XXXX): ")
         usuarios[cpf] = [nome, telefone]
         print(f"Usuário cadastrado com sucesso!")
-        menu = input("Digite \"N\" para sair ou digite qualquer tecla para continuar\nDeseja cadastrar outro usuário?")
+        menu = input("Digite \"N\" para sair ou digite qualquer tecla para continuar\nDeseja cadastrar outro usuário? ")
         menu = menu.title()
 
 # Função para cadastrar um novo voo
@@ -30,7 +30,7 @@ def cadastro_voo(id):
         lugar = int(input("Digite a quantidade de lugares disponíveis: "))
         voo[id] = [origem, destino, escala, preco, lugar]
         print(f"Voo cadastrado com sucesso!")
-        menu = input("Digite \"N\" para sair ou digite qualquer tecla para continuar\nDeseja cadastrar outro voo?")
+        menu = input("Digite \"N\" para sair ou digite qualquer tecla para continuar\nDeseja cadastrar outro voo? ")
         menu = menu.title()
 
 # Função para vender passagens
@@ -42,7 +42,8 @@ def venda_passagem():
             if (voo[codigo][4] > 0):
                 print(f"Passagem vendida com sucesso para {usuarios[cpf][0]}!")
                 voo[codigo][4] -= 1
-                passageiros.append([codigo, usuarios[cpf][0]])
+                passageiros.append(cpf)
+                voos[codigo] = passageiros
             else:
                 print("Não há lugares disponíveis.")
         else:
@@ -59,15 +60,33 @@ def listar_voo(i):
     print(f"Preço: {i[1][3]}")
     print(f"Lugares disponíveis: {i[1][4]}\n")
 
+# Função para listar os passageiros
+def listar_passageiros():
+    menu = int(input("Digite o código do voo: "))
+    if (menu in voos.keys()):
+        if (len(passageiros) > 0):
+            for i in voos[menu]:
+                print(f"\nPassageiro: {usuarios[i][0]}")
+                print(f"CPF: {i}")
+        else:
+            print("Não há passageiros cadastrados nesse voo.")
+            print("Digite \"Sair\" para encerrar o programa.")
+            print("Digite \"Menu\" para consultar o menu de opções novamente.")    
+    else:
+        print("\nNão há passageiros cadastrados nesse voo.")
+        print("Digite \"Sair\" para encerrar o programa.")
+        print("Digite \"Menu\" para consultar o menu de opções novamente.")
+
 # Função para mostrar o menu
-def menu():
+def listar_menu():
     print("// Sistema de Passagem //")
     print("1. Cadastrar um novo usuário.")
     print("2. Cadastrar um novo voo.")
-    print("3. Consultar um voo.")
-    print("4. Informar o voo com menor escala.")
-    print("5. Venda de passagem.")
+    print("3. Venda de passagem.")
+    print("4. Cancelamento de passagem.")
+    print("5. Consultar um voo.")
     print("6. Listar os passageiros em um voo.")
+    print("7. Informar o voo com menor escala.")
     print("Digite \"Sair\" para encerrar o programa.\nDigite \"Menu\" para consultar o menu de opções novamente.")
 
 # Função para consultar voos
@@ -76,12 +95,7 @@ def consulta_voo(consulta):
         codigo = int(input("Digite o código do voo: "))
         if (codigo in voo.keys()):
             print("\n// Voo //")
-            print(f"Código do Voo: {codigo}")
-            print(f"Origem: {voo[codigo][0]}")
-            print(f"Destino: {voo[codigo][1]}")
-            print(f"Escalas: {voo[codigo][2]}")
-            print(f"Preço: {voo[codigo][3]}")
-            print(f"Lugares disponíveis: {voo[codigo][4]}\n")
+            listar_voo((codigo, voo[codigo]))
         else:
             print("Voo não encontrado.")
     elif (consulta == "2"):
@@ -112,30 +126,55 @@ def menor_escala(origem, destino):
     print(f"// Voo com menor escala //")
     listar_voo(voo_menor_escala)
 
+# Função para cancelar passagens
+def cancelamento_passagem():
+    cpf = input("Digite o CPF do passageiro: ")
+    if (cpf in passageiros):
+        codigo = int(input("Digite o código do voo: "))
+        if (codigo in voo.keys()):
+                print(f"Passagem cancelada com sucesso para {usuarios[cpf][0]}!")
+                voo[codigo][4] += 1
+                passageiros.remove(cpf)
+                voos[codigo] = passageiros
+        else:
+            print("Voo não encontrado.")
+    else:
+        print("Usuário não encontrado.")
+
 # Menu de opções
-menu()
+menu = ""
+listar_menu()
 while (menu != "Sair"):
     menu = input("< Menu >: ")
     menu = menu.title()
 
     if (menu != "Sair"):
         if (menu == "Menu"):
-            menu()
+            listar_menu()
         elif (menu == "1"):
             cadastro_usuario()
         elif (menu == "2"):
             cadastro_voo(id)
             id += 1
         elif (menu == "3"):
+            print("\n// Venda de Passagem //")
+            venda_passagem()
+        elif (menu == "4"):
+            print("\n// Cancelamento de Passagem //")
+            cancelamento_passagem()
+        elif (menu == "5"):
             consulta = ""
             while (consulta != "Sair"):
                 print("O voo pode ser consultado pelo:\n1. Código do voo;\n2. Cidade de origem;\n3. Cidade de destino.")
                 print("Digite o número correspondente.\nDigite \"Sair\" para encerrar o programa.")
                 consulta = input("< Consulta >: ")
                 consulta_voo(consulta)
-        elif (menu == "4"):
+        elif (menu == "6"):
+            print("\n// Passageiros //")
+            listar_passageiros()
+        elif (menu == "7"):
             menu_escala = ""
-            print("// Voo com menor escala //")
+            print("\n// Voo com menor escala //")
             print("O voo pode ser consultado pela cidade de origem e destino.")
             print("Digite \"Sair\" para encerrar o programa.")
             while (menu_escala != "Sair"):
@@ -147,6 +186,3 @@ while (menu != "Sair"):
 
                 menu_escala = input("Digite \"Sair\" para encerrar o programa.\nDigite qualquer tecla para continuar: ")
                 menu_escala = menu_escala.title()
-        elif (menu == "5"):
-            print("// Venda de Passagem //")
-            venda_passagem()
